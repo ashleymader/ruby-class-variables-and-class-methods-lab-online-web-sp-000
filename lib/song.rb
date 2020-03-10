172f0e8fa00b26dcc4e7b772d4473f5d981b44d3
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-10 17:03:32 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+   @name = name 
+   @artist = artist
+   @genre = genre
+   @@count += 1
+   @@genres << genre
+   @@artists << artist
+  end
+  
+  @@count = 0
+  def self.count
+    @@count
+  end
+  
+  
+  @@genres = []
+  def self.genres
+    genres.uniq!
+  end
+  
+  @@artists = []
+  def self.artists
+    artists.uniq!
+  end
+  
+  
+end
